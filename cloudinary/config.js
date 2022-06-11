776a6bb14a66e6148e2acabe7500fcdaa2748085
Lam Hoa <hoa.lam201@gmail.com>
2022-06-11 23:55:25 +0700

    initial commit

---
+++ cloudinary/config.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from "cloudinary"
+
+export default cloudinary.config({
+    cloud_name: "cockbook",
+    api_key: "362125891568421",
+    api_secret: "kR3bk36ysLWcYuKLy-MN9otXogM",
+    secure: true
+})
